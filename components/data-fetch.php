7d36a41d586600efd6ef498aabb2e3e6543ab43e
Andres <andimier@gmail.com>
2021-01-23 17:51:00 -0500

    BE - sort renders data by year DESC

--- components/data-fetch.php
+++ components/data-fetch.php
@@ -1,9 +1,19 @@
 <?php
     class Portfolio {
+        public function sort_data($a, $b) {
+            return $b->year - $a->year;
+        }
+
         private function get_all_renders_data() {
             $data = file_get_contents('data/renders-data.json');
+            $decoded_data = json_decode($data);
 
-            return json_decode($data);
+            // sort by year desc
+            usort($decoded_data, function ($a, $b) {
+                return $b->year - $a->year;
+            });
+
+            return $decoded_data;
         }
 
         private function parse_info($info) {
