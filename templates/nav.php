7fa8fc23ae58a983fdacb648f4586b4a9b010d20
Andres <andimier@gmail.com>
2021-02-06 19:05:43 -0500

    FE - adjust nav styles for the nav

--- templates/nav.php
+++ templates/nav.php
@@ -1,8 +1,8 @@
 <nav class="navbar">
     <?php if (!$is_index): ?>
-        <a class="nav_link" href="./" target="_self">Portafolio</a>
+        <a class="nav-link" href="./" target="_self">Portafolio</a>
     <? endif; ?>
     <!-- <a class="nav_link" href="02_nosotros.html" target="_self">Nosotros</a> -->
 
-    <a class="nav_link" href="#contact-info-section" target="_self">Contacto</a>
+    <a class="nav-link" href="#contact-info-section" target="_self">Contacto</a>
 </nav>
