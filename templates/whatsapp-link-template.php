29d3546ce7c95ec1c6ffa8878b3fa82f1cc2f7cc
Andrés Montaña <andimier@gmail.com>
2021-09-25 19:21:42 -0500

    FE - modify whatsapp logo asset size

--- templates/whatsapp-link-template.php
+++ templates/whatsapp-link-template.php
@@ -1,5 +1,5 @@
 <div class="whatsapp-link-container <?php echo $link_class ?? ''; ?>">
     <a href="https://wa.me/573132549507" target="_blank">
-        <img src="assets/WhatsApp_Logo.png" />
+        <img src="assets/whatsapp-logo_50x50.png" />
     </a>
 </div>
