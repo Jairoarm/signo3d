2a0eea4fa954c5b0f707294aed57fed8293645d4
Andrés Montaña <andimier@gmail.com>
2023-06-06 22:23:36 -0500

    print php version check

--- components/data-template-parser.php
+++ components/data-template-parser.php
@@ -1,87 +1,88 @@
 <?php
-    function get_portfolio_renders_info() {
-        $renders_api = new RendersDataApi();
-        $renders_data = $renders_api->get_all_renders_data();
+    echo phpversion();
+    // function get_portfolio_renders_info() {
+    //     $renders_api = new RendersDataApi();
+    //     $renders_data = $renders_api->get_all_renders_data();
 
-        $portfolio = new Portfolio($renders_data);
+    //     $portfolio = new Portfolio($renders_data);
 
-        return [
-            "data" => $portfolio->get_renders_info(),
-            "portfolio" => $portfolio,
-            "template" => "portfolio.php"
-        ];
-    }
+    //     return [
+    //         "data" => $portfolio->get_renders_info(),
+    //         "portfolio" => $portfolio,
+    //         "template" => "portfolio.php"
+    //     ];
+    // }
 
-    function get_project_data($project_name) {
-        $renders_api = new RendersDataApi();
-        $renders_data = $renders_api->get_all_renders_data();
+    // function get_project_data($project_name) {
+    //     $renders_api = new RendersDataApi();
+    //     $renders_data = $renders_api->get_all_renders_data();
 
-        $project = new Project($renders_data);
-        $project_info = $project->get_project_info($project_name);
+    //     $project = new Project($renders_data);
+    //     $project_info = $project->get_project_info($project_name);
 
-        return [
-            "data" => $project_info,
-            "template" => "project.php"
-        ];
-    }
+    //     return [
+    //         "data" => $project_info,
+    //         "template" => "project.php"
+    //     ];
+    // }
 
-    function get_page_config_info($page_name) {
-        $page_api = new PageDataApi();
+    // function get_page_config_info($page_name) {
+    //     $page_api = new PageDataApi();
 
-        return $page_api->get_page_data($page_name);
-    }
+    //     return $page_api->get_page_data($page_name);
+    // }
 
-    function get_section_data() {
-        return [
-            "template" => "section.php"
-        ];
-    }
+    // function get_section_data() {
+    //     return [
+    //         "template" => "section.php"
+    //     ];
+    // }
 
-    function get_section_type($resource) {
-        $sections = [
-            'nosotros'
-        ];
+    // function get_section_type($resource) {
+    //     $sections = [
+    //         'nosotros'
+    //     ];
 
-        if ($resource == null) {
-            return 'index';
-        }
+    //     if ($resource == null) {
+    //         return 'index';
+    //     }
 
-        if (in_array($resource, $sections)) {
-            return 'section';
-        }
+    //     if (in_array($resource, $sections)) {
+    //         return 'section';
+    //     }
 
-        return 'project';
-    }
+    //     return 'project';
+    // }
 
-    function get_page_name($resource) {
-        $sections_map = [
-            'nosotros' => 'us'
-        ];
+    // function get_page_name($resource) {
+    //     $sections_map = [
+    //         'nosotros' => 'us'
+    //     ];
 
-        if ($resource == null) {
-            return 'inicio';
-        }
+    //     if ($resource == null) {
+    //         return 'inicio';
+    //     }
 
-        return $sections_map[$resource] ?? '';
-    }
+    //     return $sections_map[$resource] ?? '';
+    // }
 
-    try {
+    // try {
 
-        $page_data_map = [
-            "index" => "get_portfolio_renders_info",
-            "section" => "get_section_data",
-            "project" => "get_project_data"
-        ];
+    //     $page_data_map = [
+    //         "index" => "get_portfolio_renders_info",
+    //         "section" => "get_section_data",
+    //         "project" => "get_project_data"
+    //     ];
 
-        $page = $_GET['project-name'] ?? null;
-        $section_type = get_section_type($page);
-        $page_data = $page_data_map[$section_type]($page);
+    //     $page = $_GET['project-name'] ?? null;
+    //     $section_type = get_section_type($page);
+    //     $page_data = $page_data_map[$section_type]($page);
 
-        $template = $page_data['template'];
+    //     $template = $page_data['template'];
 
-        $page_config_data = get_page_config_info(get_page_name($page));
-    } catch (Exception $e) {
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
-    }
+    //     $page_config_data = get_page_config_info(get_page_name($page));
+    // } catch (Exception $e) {
+    //     echo 'Caught exception: ',  $e->getMessage(), "\n";
+    // }
 
 ?>
